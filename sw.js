7736492fced6f22ab8ef20dc5239f289758ba2bb
amourrrrr <2517746776@qq.com>
2026-06-14 23:19:17 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'kaoyan-checkin-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/icons/icon.svg',
+  '/icons/apple-icon-180x180.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cached => cached || fetch(evt.request))
+  );
+});
